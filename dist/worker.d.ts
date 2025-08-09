73780c7be961fc1999456fab4a5af1b6225260bb
Bubunyo Nyavor <kidd.bubu@gmail.com>
2025-08-09 12:19:31 +0000

    ship dist folder

---
+++ dist/worker.d.ts
@@ -0,0 +1,18 @@
+import { WorkFunction, WorkerOptions, ClientConfig } from './types';
+export declare class Worker {
+    private client;
+    private workMap;
+    private queue;
+    private interval;
+    private running;
+    private shutdownPromise;
+    private timeoutId;
+    constructor(clientConfig?: ClientConfig, options?: WorkerOptions);
+    register(jobClass: string, workFunc: WorkFunction): void;
+    work(): Promise<void>;
+    workOne(): Promise<boolean>;
+    shutdown(): Promise<void>;
+    private workLoop;
+    private processJob;
+}
+//# sourceMappingURL=worker.d.ts.map
